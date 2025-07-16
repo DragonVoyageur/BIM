21f3f752b76d14e8560211e176fdb146a3588caa
JJSax <5084938+JJSax@users.noreply.github.com>
2025-07-16 13:24:48 -0600

    Format startup to be more readable

--- Startup/StartBIM.lua
+++ Startup/StartBIM.lua
@@ -1,10 +1,15 @@
-ProjectName='BIM'
-Programs={'Inventory','Crafter','Settings'}
+ProjectName = 'BIM'
+Programs = { 'Inventory', 'Crafter', 'Settings' }
 for index, value in ipairs(Programs) do
     multishell.setTitle(
-    multishell.launch(
-        {Um=require('/'..ProjectName..'/Functions/UiManager'),Vs=require('/'..ProjectName..'/Functions/VariableStorage'),require=require,multishell=multishell},
-        '/'..ProjectName..'/'..value..'Manager.lua'),
-    value
+        multishell.launch(
+            {
+                Um = require('/' .. ProjectName .. '/Functions/UiManager'),
+                Vs = require('/' .. ProjectName .. '/Functions/VariableStorage'),
+                require = require,
+                multishell = multishell
+            },
+            '/' .. ProjectName .. '/' .. value .. 'Manager.lua'),
+        value
     )
 end
